4e1ac931c16e28d4c77459644a9a9cf86b85a509
vanita-r <68659221+vanita-r@users.noreply.github.com>
2021-05-29 20:38:23 -0400

    Add files via upload

---
+++ screens/SettingsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Touchable, Alert, Modal, KeyboardAvoidingView, ScrollView } from 'react-native';
+import firebase from "firebase";
+
+export default class SettingsScreens extends React.Component{
+    constructor(){
+        super()
+    }
+}
+
+const styles = StyleSheet.create({ 
+})
